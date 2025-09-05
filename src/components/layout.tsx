03a4e5031a537fc1eb288a6d638d97b1d4062c6d
Minal Kulkarni <minalk97@gmail.com>
2025-09-05 23:00:16 +0100

    Modify Footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import { type PropsWithChildren } from "react";
 import Header from "./header";
+import { Link } from "react-router-dom";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
@@ -11,7 +12,17 @@ const Layout = ({ children }: PropsWithChildren) => {
 
       <footer className="backdrop-blur border-t py-12 supports-[backdrop-filter]-bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-          <p>Made by Minal Kulkarni</p>
+          <div>Data Source: </div>
+          <div>
+            Weather data provided by
+            <Link
+              to={`https://openweathermap.org/`}
+              className="ml-2 text-blue-600 underline"
+              target="_blank"
+            >
+              OpenWeather
+            </Link>
+          </div>
         </div>
       </footer>
     </div>
